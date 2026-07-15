ad5ad246a3877600840dda3882eb35ec85d466a6
BL-MariaSelvam <maria.selvam@bridgelabz.com>
2026-07-15 15:36:18 +0530

    SpringBootApplication

---
+++ src/main/java/com/demo/jenkinslearning/JenkinsLearningApplication.java
@@ -0,0 +1,13 @@
+package com.demo.jenkinslearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsLearningApplication.class, args);
+    }
+
+}
